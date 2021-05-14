f25cc0117ef4c982fb0f00604d5a319b67de1f04
saipratap99 <122005067@sastra.ac.in>
2021-05-14 20:45:05 +0530

    writing python code inside templates {% %} for logic/loop/condition {{ }} for variable

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,8 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+  {
+    'author': 'Sai Pratap',
+    'title' : 'First post',
+    'content': "Sai pratap's first post",
+    'date_posted' : '27th Aug 2021'
+  },
+  {
+    'author': 'Tarun',
+    'title' : 'First post',
+    'content': "Tarun's first post",
+    'date_posted' : '13th Aug 2021'
+  }
+]
+
 def home(request):
-  return render(request,'blog_app/home.html')
+  context = {
+    'posts': posts,
+    'title' : 'home'
+  }
+  return render(request,'blog_app/home.html',context)
 
 def about(request):
   return render(request,'blog_app/about.html')
