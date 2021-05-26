7d93f4bbcecdcd0bed9dc1077405321718cf980c
saipratap99 <122005067@sastra.ac.in>
2021-05-26 17:16:16 +0530

    After adding post table and ran makemigrations

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  # timezone.now is a function but is called only when the data is inserted
+  date_posted = models.DateTimeField(default=timezone.now)
+  auth = models.ForeignKey(User, on_delete=models.CASCADE)
