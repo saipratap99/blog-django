e705ab6899ac791b1ba1d9fe74e837d6c540bc51
saipratap99 <122005067@sastra.ac.in>
2021-05-28 01:10:30 +0530

    Added extra field by extending user form

--- users/views.py
+++ users/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
+
 # Create your views here.
 def register(request):
   if request.method == 'POST':
     # if request is post then it posts the data
-    form = UserCreationForm(request.POST)
+    form = UserRegisterForm(request.POST)
     if form.is_valid():
+      form.save()
       username = form.cleaned_data.get('username')
       messages.success(request,f"Account created from {username}")
-    return redirect('blog-home')
+      return redirect('blog-home')
   else:
-    form = UserCreationForm()
-    return render(request,"users/register.html",{ 'form': form })
+    form = UserRegisterForm()
+  return render(request,"users/register.html",{ 'form': form })
