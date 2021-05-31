7f3092ed2a3b810078ce299de8040c58a1a733e3
saipratap99 <122005067@sastra.ac.in>
2021-05-31 13:38:40 +0530

    now user can update their details

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 # inherit class to add extra fields
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +11,15 @@ class UserRegisterForm(UserCreationForm):
   class Meta:
     model = User
     fields = ['email','username','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+  email = forms.EmailField()
+
+  class Meta:
+    model = User
+    fields = ['email','username']
+
+class ProfileUpdateForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    fields = ['image']
