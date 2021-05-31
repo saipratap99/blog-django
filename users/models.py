c82e6b77c7f316345cb7b4ad9f9dfcc4296fb8d7
saipratap99 <122005067@sastra.ac.in>
2021-05-31 13:50:59 +0530

    resizing image done

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 # Create your models here.
 class Profile(models.Model):
   user = models.OneToOneField(User,on_delete=models.CASCADE)
@@ -8,3 +8,16 @@ class Profile(models.Model):
 
   def __str__(self):
     return f"{self.user.username} Profile"
+
+  # overrding save
+
+  def save(self):
+    super().save()
+
+    img = Image.open(self.image.path)
+
+    if( img.height > 300 or img.width > 300):
+      out_size = (300,300)
+      img.thumbnail(out_size)
+      # save to the same path
+      img.save(self.image.path)
