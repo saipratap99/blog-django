f234c4d1c7be89b9fc79d37ae4fb41fea0498f6b
saipratap99 <122005067@sastra.ac.in>
2021-06-08 16:10:50 +0530

    Added mixins for login required

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
           ListView,
           DetailView,
@@ -28,7 +29,7 @@ class PostDetailView(DetailView):
   # context key name is object
   model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
   # looks for # <app_name>/<model>_form.html
   # context key name is object
   model = Post
