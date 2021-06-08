496808437e84a9b5cb586a760f7aaa19c7a9acd9
saipratap99 <122005067@sastra.ac.in>
2021-06-08 16:45:30 +0530

    FEAT:Post can be deleted

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import (
           ListView,
           DetailView,
           CreateView,
-          UpdateView)
+          UpdateView,
+          DeleteView)
 from .models import Post
 # Create your views here.
 
@@ -40,7 +41,7 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     form.instance.auth = self.request.user
     return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
   # uses same post_form.html
   # looks for # <app_name>/<model>_form.html
   # context key name is object
@@ -50,6 +51,27 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
   def form_valid(self,form):
     form.instance.auth = self.request.user
     return super().form_valid(form)
+
+  # in order to update the post only by the author
+  def test_func(self):
+    post = self.get_object()  # current post
+    if self.request.user == post.auth:
+      return True
+    else:
+      return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+  # post_confirm_delete.html
+  model = Post
+  success_url = '/'
+
+  # in order to delete the post only by the author
+  def test_func(self):
+    post = self.get_object()  # current post
+    if self.request.user == post.auth:
+      return True
+    else:
+      return False
 
 
 
